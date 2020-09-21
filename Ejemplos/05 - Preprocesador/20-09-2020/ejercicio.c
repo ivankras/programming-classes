26cd4fae63eeb9f5460181d05465a6c9935953f0
Ivan Krasowski <ivankras@hotmail.com>
2020-09-20 21:51:05 -0300

    Clase 20/9

---
+++ Ejemplos/05 - Preprocesador/20-09-2020/ejercicio.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define DEBUG
+
+#define FALSE 0
+#define TRUE !FALSE
+
+int main() {
+    
+    int a = 3;
+
+    a++;
+    #ifdef DEBUG
+        printf("Valor intermedio de a: %d\n", a);
+    #endif
+
+    a++;
+    #ifdef DEBUG
+        printf("Valor intermedio de a: %d\n", a);
+    #endif
+
+    #ifndef DEBUG
+    printf("Valor final de a: %d", a);
+    #endif
+
+    // Se compila el printf si A está definido y B no lo está
+    #ifdef A
+        #ifndef B
+            printf("Hola");
+        #endif
+    #endif
+
+    return 0;
+}
